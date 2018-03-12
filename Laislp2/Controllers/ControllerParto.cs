bff3ac9837e4dd9a44fc4ae6641c15046f027373
Joyce <joycequintino11@gmail.com>
2018-03-12 17:17:21 -0300

    Criação do controle parto

--- Laislp2/Controllers/ControllerParto.cs
+++ Laislp2/Controllers/ControllerParto.cs
@@ -1,7 +1,80 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Laislp2.Models;
+using System.Linq;
+
 namespace Laislp2.Controllers
 {
-    public class ControllerParto
+     [Route("Nascimento/Consulta")]
+    public class ControllerParto : Controller
     {
-        
+        private readonly LaisContext _context;
+
+        [HttpGet]
+        public IEnumerable<Parto> GetAll()
+        {
+            return _context.Partos.ToList();
+        }
+
+        [HttpGet("{id}", Name = "Parto")]
+        public IActionResult GetById(int id)
+        {
+            var par = _context.Partos.FirstOrDefault(p => p.Id == id);
+            if (par == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(par);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Parto pa)
+        {
+            if (pa == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Partos.Add(pa);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("Parto", new { id = pa.Id },pa);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Parto pa)
+        {
+            if (pa == null || pa.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var par = _context.Partos.FirstOrDefault(p => p.Id == id);
+            if (par == null)
+            {
+                return NotFound();
+            }
+
+            par.LocalNasci = pa.LocalNasci;
+            par.Horario = pa.Horario;
+
+            _context.Partos.Update(par);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var par = _context.Partos.FirstOrDefault(p => p.Id == id);
+            if (par == null)
+            {
+                return NotFound();
+            }
+
+            _context.Partos.Remove(par);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
     }
 }
