d0d6e058bf3c5b3b3a5071d69f5ff90489d4eb63
Joyce <joycequintino11@gmail.com>
2018-03-12 16:52:12 -0300

    Criação dos controles da consulta e da mãe

--- Laislp2/Controllers/ControllerConsulta.cs
+++ Laislp2/Controllers/ControllerConsulta.cs
@@ -1,7 +1,82 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Laislp2.Models;
+using System.Linq;
+
 namespace Laislp2.Controllers
 {
-    public class ControllerConsulta
+
+    [Route("Nascimento/Consulta")]
+    public class ControllerConsulta : Controller
     {
-        
+        private readonly LaisContext _context;
+
+        [HttpGet]
+        public IEnumerable<Consulta> GetAll()
+        {
+            return _context.Consultas.ToList();
+        }
+
+        [HttpGet("{id}", Name = "Consulta")]
+        public IActionResult GetById(int id)
+        {
+            var con = _context.Consultas.FirstOrDefault(co => co.Id == id);
+            if (con == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(con);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Consulta con)
+        {
+            if (con == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Consultas.Add(con);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("Consulta", new { id = con.Id }, con);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Consulta con)
+        {
+            if (con == null || con.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var cons = _context.Consultas.FirstOrDefault(co => co.Id == id);
+            if (cons == null)
+            {
+                return NotFound();
+            }
+
+            cons.Gravidez = con.Gravidez;
+            cons.TempoGestacao = con.TempoGestacao;
+            cons.TipoParto = con.TipoParto;
+
+            _context.Consultas.Update(cons);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var cons = _context.Consultas.FirstOrDefault(co => co.Id == id);
+            if (cons == null)
+            {
+                return NotFound();
+            }
+
+            _context.Consultas.Remove(cons);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
     }
 }
--- Laislp2/Controllers/ControllerMae.cs
+++ Laislp2/Controllers/ControllerMae.cs
@@ -1,7 +1,85 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Laislp2.Models;
+using System.Linq;
+
 namespace Laislp2.Controllers
 {
-    public class ControllerMae
+    [Route("Nascimento/Mae")]
+    public class ControllerMae : Controller
     {
-        
+        private readonly LaisContext _context;
+
+        [HttpGet]
+        public IEnumerable<Mae> GetAll()
+        {
+            return _context.Maes.ToList();
+        }
+
+        [HttpGet("{id}", Name = "Mae")]
+        public IActionResult GetById(int id)
+        {
+            var mae = _context.Maes.FirstOrDefault(m => m.Id == id);
+            if (mae == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(mae);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Mae mae)
+        {
+            if (mae == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Maes.Add(mae);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("Mae", new { id = mae.Id }, mae);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Mae mae)
+        {
+            if (mae == null || mae.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var ma = _context.Maes.FirstOrDefault(m => m.Id == id);
+            if (ma == null)
+            {
+                return NotFound();
+            }
+
+            ma.Nome = mae.Nome;
+            ma.IdadeMae = mae.IdadeMae;
+            ma.escolaridade = mae.escolaridade;
+            ma.EstadoCivil = mae.EstadoCivil;
+            ma.QuantFilhosMortos = mae.QuantFilhosMortos;
+            ma.QuantFilhosVivos = mae.QuantFilhosVivos;
+
+
+            _context.Maes.Update(ma);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var ma = _context.Maes.FirstOrDefault(m => m.Id == id);
+            if (ma == null)
+            {
+                return NotFound();
+            }
+
+            _context.Maes.Remove(ma);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
     }
 }
