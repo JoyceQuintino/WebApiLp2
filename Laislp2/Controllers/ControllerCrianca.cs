6fbbc45b7ac6db57e3f16054206123fd274d4cb4
Joyce <joycequintino11@gmail.com>
2018-03-11 22:11:00 -0300

    Criação da entidade Criança

--- Laislp2/Controllers/ControllerCrianca.cs
+++ Laislp2/Controllers/ControllerCrianca.cs
@@ -8,6 +8,75 @@ namespace Laislp2.Controllers
     [Route("Nascimento/Crianca")]
     public class ControllerCrianca : Controller
     {
-        
+        private readonly LaisContext _context;
+
+        [HttpGet]
+        public IEnumerable<Crianca> GetAll()
+        {
+            return _context.Criancas.ToList();
+        }
+
+        [HttpGet("{id}", Name = "Crianca")]
+        public IActionResult GetById(int id)
+        {
+            var crianca = _context.Criancas.FirstOrDefault(c => c.Id == id);
+            if (crianca == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(crianca);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Crianca crianca)
+        {
+            if (crianca == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Criancas.Add(crianca);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("Crianca", new { id = crianca.Id }, crianca);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Crianca crianca)
+        {
+            if (crianca == null || crianca.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var cri = _context.Criancas.FirstOrDefault(c => c.Id == id);
+            if (cri == null)
+            {
+                return NotFound();
+            }
+
+            cri.Peso = crianca.Peso;
+            cri.Sexo = crianca.Sexo;
+            cri.Apgar1 = crianca.Apgar1;
+            cri.Apgar2 = crianca.Apgar2;
+
+            _context.Criancas.Update(cri);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var cri = _context.Criancas.FirstOrDefault(c => c.Id == id);
+            if (cri == null)
+            {
+                return NotFound();
+            }
+
+            _context.Criancas.Remove(cri);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
     }
 }
